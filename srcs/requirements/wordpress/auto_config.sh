f14331b90bbaafcf2951e1863178a3f298cf2c47
Theotime <theo.eliet@gmail.com>
2023-05-30 18:50:21 +0200

    wordpress working

--- srcs/requirements/wordpress/auto_config.sh
+++ srcs/requirements/wordpress/auto_config.sh
@@ -1,10 +1,26 @@
 
 echo "Wordpress tries to connect to $DB_NAME as $DB_USER with password $DB_PASSWORD" 
+# sleep 10
 
-# if !( wp core is-installed --alow-root & [ -f "$(wp config path --allow-root)" ]); then
-wp config create	--allow-root \
-                    --dbname=$DB_NAME \
-                    --dbuser=$DB_USER \
-                    --dbpass=$DB_PASSWORD \
-                    --dbhost=mariadb:3306 \
-                    --path='/var/www/wordpress'
+
+# Configure Wordpress website
+if [ ! -f /var/www/wordpress/wp-config.php ]; then
+    wp config create	--allow-root \
+                        --dbname="$DB_NAME" \
+                        --dbuser="$DB_USER" \
+                        --dbpass="$DB_PASSWORD" \
+                        --dbhost=mariadb:3306 \
+                        --path='/var/www/html/wordpress'
+else 
+    echo "wp-config already exists"
+fi
+
+# Launch php 
+mkdir /run/php
+/usr/sbin/php-fpm7.3 -F
+
+while [ 1 ]
+do
+    foo
+    sleep 2
+done
