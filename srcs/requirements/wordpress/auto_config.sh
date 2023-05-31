aa99f291f9833f865333d5e07889e72ab5fefb8d
Theotime <theo.eliet@gmail.com>
2023-05-31 17:30:24 +0200

    feat: added php listen on wordpress container + wordpress auto set-up

--- srcs/requirements/wordpress/auto_config.sh
+++ srcs/requirements/wordpress/auto_config.sh
@@ -11,12 +11,22 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
                         --dbpass="$DB_PASSWORD" \
                         --dbhost=mariadb:3306 \
                         --path='/var/www/html/wordpress'
+
+    wp core install --allow-root \
+                    --url=$DOMAIN_NAME/ \
+                    --title=$WP_TITLE \
+                    --admin_user=$WP_ADMIN_USR \
+                    --admin_password=$WP_ADMIN_PWD \
+                    --admin_email=$WP_ADMIN_EMAIL \
+                    --skip-email \
+                    --path='/var/www/html/wordpress'
 else 
     echo "wp-config already exists"
 fi
 
-# Launch php 
+# Config and launch php 
 mkdir /run/php
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 /usr/sbin/php-fpm7.3 -F
 
 while [ 1 ]
